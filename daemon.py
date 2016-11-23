fc9ba9c2dbd7d327ead3458dccc3f16499e363bb
Xero-Hige <gaston.martinez.90@gmail.com>
2016-11-23 20:55:42 -0300

    Added template

--- daemon.py
+++ daemon.py
@@ -4,9 +4,11 @@ import cx_Oracle
 #con = cx_Oracle.connect('owner_rafam/ownerdba@154.2.6.107:1521/MEZEIZA')
 
 def run():
+    alerts = []
     for file_name in os.listdir("rules/"):
         with open("rules/"+file_name) as my_file:
             alert_type = my_file.readline().rstrip()
+            alert_name = my_file.readline().rstrip()
             alert_query = my_file.readline().rstrip()
             alert_results_tags = my_file.readline().rstrip().split(",")
 
@@ -14,10 +16,12 @@ def run():
             #cur.execute(alert_query)
             cur = [(12,13),(15,192),(8,88)]
             for result in cur:
-                print("Alert: "+alert_type)
-                for i in range(len(alert_results_tags)):
-                    print("\t"+alert_results_tags[i]+" : "+str(result[i]))
-
-            #cur.close()
+                alert = {}
+                alert["TYPE"] = alert_type
+                alert["NAME"] = alert_name
+                tags = [(alert_results_tags[i],result[i]) for i in range(len(result))]
+                alert["TAGS"] = tags
+                alerts.append(alert)
+            cur.close()
 
 run()
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+import daemon
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def alerts():
+    alerts = daemon.run() #FIXME: Rename
+    return render_template("index.html",alerts=alerts)

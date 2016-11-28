e765a4af25f22185998fe32a2798b9b043a2956c
Xero-Hige <gaston.martinez.90@gmail.com>
2016-11-27 22:06:19 -0300

    Added boostrap

--- dummy_alert_generator.py
+++ dummy_alert_generator.py
@@ -1,8 +1,8 @@
 import os
-import cx_Oracle
+from random import randint, choice  # import cx_Oracle
 
-con = cx_Oracle.connect('CONECTION')
 
+# con = cx_Oracle.connect('CONECTION')
 
 def generate_alerts():
     alerts = []
@@ -10,18 +10,20 @@ def generate_alerts():
         with open("rules/" + file_name) as my_file:
             alert_type = my_file.readline().rstrip()
             alert_name = my_file.readline().rstrip()
+            alert_description = my_file.readline().rstrip()
             alert_query = my_file.readline().rstrip()
             alert_results_tags = my_file.readline().rstrip().split(",")
 
-            cur = con.cursor()
-            cur.execute(alert_query)
-            # cur = [(12,13),(15,192),(8,88)]
+            # cur = con.cursor()
+            # cur.execute(alert_query)
+            cur = [[randint(0, 500) for x in range(len(alert_results_tags))] for i in range(randint(15, 30))]
             for result in cur:
                 alert = {}
-                alert["TYPE"] = alert_type
+                alert["TYPE"] = choice(["red", "green", "blue", "yellow"])  # alert_type
                 alert["NAME"] = alert_name
+                alert["DESC"] = alert_description
                 tags = [(alert_results_tags[i], result[i]) for i in range(len(result))]
                 alert["TAGS"] = tags
                 alerts.append(alert)
-            cur.close()
+                # cur.close()
     return alerts
--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-import alert_generator
+from dummy_alert_generator import generate_alerts
+
+groups = {"red": "list-group-item-danger", "green": "list-group-item-success", "blue": "list-group-item-info",
+          "yellow": "list-group-item-warning"}
+
 
 @app.route('/')
 def alerts():
-    alerts = alert_generator.generate_alerts()
-    return render_template("index.html",alerts=alerts)
+    alerts = generate_alerts()
+    return render_template("index.html", alerts=alerts, groups=groups)

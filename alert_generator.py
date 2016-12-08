286ab6ad9f443986ab740810816c8889f37c7f35
Xero-Hige <gaston.martinez.90@gmail.com>
2016-12-08 13:56:11 -0300

    Added missing tag to non dummy generator

--- alert_generator.py
+++ alert_generator.py
@@ -1,4 +1,5 @@
 import os
+
 import cx_Oracle
 
 con = cx_Oracle.connect('CONECTION')
@@ -10,6 +11,7 @@ def generate_alerts():
         with open("rules/" + file_name) as my_file:
             alert_type = my_file.readline().rstrip()
             alert_name = my_file.readline().rstrip()
+            alert_description = my_file.readline().rstrip()
             alert_query = my_file.readline().rstrip()
             alert_results_tags = my_file.readline().rstrip().split(",")
 
@@ -17,9 +19,7 @@ def generate_alerts():
             cur.execute(alert_query)
             # cur = [(12,13),(15,192),(8,88)]
             for result in cur:
-                alert = {}
-                alert["TYPE"] = alert_type
-                alert["NAME"] = alert_name
+                alert = {"TYPE": alert_type, "NAME": alert_name, "DESC": alert_description}
                 tags = [(alert_results_tags[i], result[i]) for i in range(len(result))]
                 alert["TAGS"] = tags
                 alerts.append(alert)

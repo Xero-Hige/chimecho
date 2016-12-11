46e5a35a6bf92e35fa737ca10342804dcb28a6db
Xero-Hige <gaston.martinez.90@gmail.com>
2016-12-10 22:47:05 -0300

    Added list alerts by type

--- alerts_reader.py
+++ alerts_reader.py
@@ -6,18 +6,36 @@ ALERTS_PATH = "alerts/"
 
 def load_alerts():
     alerts = []
-    for file_name in os.listdir(ALERTS_PATH):
+    alerts_files = list_alerts_types()
+
+    for file_name in alerts_files:
         alerts += read_alerts_file(file_name)
 
     return alerts
 
 
+def list_alerts_types():
+    alerts_files = []
+    for file_name in os.listdir(ALERTS_PATH):
+        alerts_files.append(file_name)
+    return alerts_files
+
+
 def read_alerts_file(file_name):
     alerts = []
-    with open(os.path.join(ALERTS_PATH, file_name)) as my_file:
+
+    alert_path = os.path.join(ALERTS_PATH, file_name)
+
+    if not os.path.isfile(alert_path):
+        return alerts
+
+    with open(alert_path) as my_file:
         reader = csv.reader(my_file)
 
         for alert in reader:
+            if len(alert) < 3:
+                continue
+
             alert_type = alert[0].lower()
             alert_name = file_name
             alert_description = alert[1]

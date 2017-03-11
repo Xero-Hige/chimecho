988f7c83edb86e9e1602b4be66e3f08e8399f81e
Xero-Hige <gaston.martinez.90@gmail.com>
2017-03-11 18:23:11 -0300

    Added reading alerts from the merged file

--- alerts_reader.py
+++ alerts_reader.py
@@ -4,6 +4,12 @@ import os
 from constants_config import *
 
 
+def load_enabled_alerts(enabled_alerts_dic):
+    types_acumulator = {ALERT_RED: 0}
+    alerts = read_alerts_file(ALL_ALERTS_FILE, enabled_alerts_dic, "", types_acumulator)
+    return len(alerts), alerts, types_acumulator
+
+
 def load_alerts():
     alerts = []
     alerts_files = list_alerts_types()
@@ -22,10 +28,10 @@ def list_alerts_types():
     return alerts_files
 
 
-def read_alerts_file(filename):
+def read_alerts_file(filename, filter_dic=None, base_dir=ALERTS_DIR, type_acumulator=None):
     alerts = []
 
-    alert_path = os.path.join(ALERTS_DIR, filename)
+    alert_path = os.path.join(base_dir, filename)
 
     if not os.path.isfile(alert_path):
         return alerts
@@ -38,12 +44,21 @@ def read_alerts_file(filename):
                 continue
 
             alert_type = alert[0].lower()
+
+            if filter_dic:
+                if alert_type not in filter_dic or not filter_dic[alert_type]:
+                    continue
+
+            if type_acumulator:
+                type_acumulator[alert_type] = type_acumulator.get(alert_type, 0) + 1
+
             alert_name = filename
             alert_description = alert[1]
 
             alert_dic = {"TYPE": alert_type, "NAME": alert_name, "DESC": alert_description}
             tags = [alert[i].split("|") for i in range(2, len(alert))]
             alert_dic["TAGS"] = tags
+
             alerts.append(alert_dic)
 
     return alerts
